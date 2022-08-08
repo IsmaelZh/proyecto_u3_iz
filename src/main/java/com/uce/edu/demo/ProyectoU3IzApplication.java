ecb2035dd1f7579766e665307f8c9ba6bac1867c
Ismael Zhindon <ismael.zhindon.xd@gamail.com>
2022-08-08 11:37:40 -0500

    Tarea 27

--- src/main/java/com/uce/edu/demo/ProyectoU3IzApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU3IzApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.repository.modelo.DetalleFactura;
 import com.uce.edu.demo.repository.modelo.Factura;
 import com.uce.edu.demo.service.IFacturaService;
-import com.uce.edu.demo.service.IHotelService;
 
 @SpringBootApplication
 public class ProyectoU3IzApplication implements CommandLineRunner {
@@ -29,63 +29,63 @@ public class ProyectoU3IzApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 
-		// Inner
-		LOG.info("INNER JOIN");
-		List<Factura> listafacturas = this.facturaService.buscarFacturaInnerJoin(6);
-		for (Factura f : listafacturas) {
-			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
-		}
-
-		List<Factura> listaFactura1 = this.facturaService.FacturaInnerJoin();
-		for (Factura f : listaFactura1) {
-			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
-		}
-
-		// Left
-		LOG.info("JOIN LEFT");
-		List<Factura> listaFacturaLeft = this.facturaService.buscarFacturaOuterJoinLeft(6);
-		for (Factura f : listaFacturaLeft) {
-			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
-		}
-
-		List<Factura> listaFacturaLeft1 = this.facturaService.buscarFacturaOuterJoinLeft();
-		for (Factura f : listaFacturaLeft1) {
-			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
-		}
-
-		// Right
-		LOG.info("JOIN RIGHT");
-		List<Factura> listaFacturaRigth = this.facturaService.buscarFacturaOuterJoinRight(6);
-		for (Factura f : listaFacturaRigth) {
-			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
-		}
-
-		
-//		LOG.info("RELACIONAMIENTO WHERE");
-//		List<Factura> listaFacturas = this.facturaService.buscarFacturaJoinWhere(6);
-//		for (Factura f : listaFacturas) {
+//		// Inner
+//		LOG.info("INNER JOIN");
+//		List<Factura> listafacturas = this.facturaService.buscarFacturaInnerJoin(6);
+//		for (Factura f : listafacturas) {
 //			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
 //		}
 //
-//		LOG.info("INNER JOIN EAGER/LAZY");
-//		List<Factura> listaFacturas2 = this.facturaService.buscarFacturaInnerJoin2(6);
-//		for (Factura f : listaFacturas2) {
-//			LOG.info("Factura2: " + f.getFecha() + " " + f.getNumero());
-//			for (DetalleFactura d : f.getDetalles()) {
-//				LOG.info("Detalle2: " + d);
-//			}
-//
+//		List<Factura> listaFactura1 = this.facturaService.FacturaInnerJoin();
+//		for (Factura f : listaFactura1) {
+//			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
 //		}
 //
-//		LOG.info("JOIN FETCH");
-//		List<Factura> listaFacturas3 = this.facturaService.buscarFacturaJoinFetch(6);
-//		for (Factura f : listaFacturas3) {
-//			LOG.info("Factura3: " + f.getFecha() + " " + f.getNumero());
-//			for (DetalleFactura d : f.getDetalles()) {
-//				LOG.info("Detalle3: " + d);
-//			}
-//
+//		// Left
+//		LOG.info("JOIN LEFT");
+//		List<Factura> listaFacturaLeft = this.facturaService.buscarFacturaOuterJoinLeft(6);
+//		for (Factura f : listaFacturaLeft) {
+//			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
 //		}
+//
+//		List<Factura> listaFacturaLeft1 = this.facturaService.buscarFacturaOuterJoinLeft();
+//		for (Factura f : listaFacturaLeft1) {
+//			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
+//		}
+//
+//		// Right
+//		LOG.info("JOIN RIGHT");
+//		List<Factura> listaFacturaRigth = this.facturaService.buscarFacturaOuterJoinRight(6);
+//		for (Factura f : listaFacturaRigth) {
+//			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
+//		}
+
+		
+		LOG.info("RELACIONAMIENTO WHERE");
+		List<Factura> listaFacturas = this.facturaService.buscarFacturaJoinWhere(6);
+		for (Factura f : listaFacturas) {
+			LOG.info("Factura: " + f.getFecha() + " " + f.getNumero());
+		}
+
+		LOG.info("INNER JOIN EAGER/LAZY");
+		List<Factura> listaFacturas2 = this.facturaService.buscarFacturaInnerJoin2(6);
+		for (Factura f : listaFacturas2) {
+			LOG.info("Factura2: " + f.getFecha() + " " + f.getNumero());
+			for (DetalleFactura d : f.getDetalles()) {
+				LOG.info("Detalle2: " + d);
+			}
+
+		}
+
+		LOG.info("JOIN FETCH");
+		List<Factura> listaFacturas3 = this.facturaService.buscarFacturaJoinFetch(6);
+		for (Factura f : listaFacturas3) {
+			LOG.info("Factura3: " + f.getFecha() + " " + f.getNumero());
+			for (DetalleFactura d : f.getDetalles()) {
+				LOG.info("Detalle3: " + d);
+			}
+
+		}
 
 	}
 
